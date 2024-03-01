4170c33b1724999ab02bfa4416bbec31486ddcc9
LForchini <git@lforchini.com>
2024-03-01 17:12:39 +0000

    Capability to run ASM

---
+++ src/asm_function.rs
@@ -0,0 +1,58 @@
+use mmap::MapOption::*;
+use mmap::*;
+use std::marker::Tuple;
+use std::mem;
+
+pub struct AsmFunction<Args, Ret>
+where
+    Args: Tuple,
+{
+    function: unsafe extern "C" fn(Args) -> Ret,
+    _mmap: MemoryMap,
+}
+
+impl<Args, Ret> AsmFunction<Args, Ret>
+where
+    Args: Tuple,
+{
+    pub unsafe fn new(buf: &[u8]) -> Self {
+        let mmap = MemoryMap::new(buf.len(), &[MapReadable, MapWritable, MapExecutable]).unwrap();
+        std::ptr::copy_nonoverlapping(buf.as_ptr(), mmap.data(), buf.len());
+
+        let function = mem::transmute(mmap.data());
+
+        Self {
+            function,
+            _mmap: mmap,
+        }
+    }
+}
+
+impl<Args, Ret> Fn<Args> for AsmFunction<Args, Ret>
+where
+    Args: Tuple,
+{
+    extern "rust-call" fn call(&self, args: Args) -> Self::Output {
+        unsafe { (self.function)(args) }
+    }
+}
+
+impl<Args, Ret> FnMut<Args> for AsmFunction<Args, Ret>
+where
+    Args: Tuple,
+{
+    extern "rust-call" fn call_mut(&mut self, args: Args) -> Self::Output {
+        self.call(args)
+    }
+}
+
+impl<Args, Ret> FnOnce<Args> for AsmFunction<Args, Ret>
+where
+    Args: Tuple,
+{
+    type Output = Ret;
+
+    extern "rust-call" fn call_once(self, args: Args) -> Self::Output {
+        self.call(args)
+    }
+}
